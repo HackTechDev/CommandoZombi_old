e7d12dc10974771824b6839f5eaf3d5fdedb6256
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2018-05-08 19:22:06 +0200

    Fix pseudo

--- js/About.js
+++ js/About.js
@@ -18,7 +18,7 @@ CommandoZombi.About.prototype = {
     	this.splash = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
     	this.splash.anchor.setTo(0.5);
 
-        var text = "Developped by Sam\nTested by Remy, Quentin";
+        var text = "Developped by Sam\nTested by WOoOinux, Quentin";
         var style = { font: "30px Arial", fill: "#000", align: "center" };
         var ts = this.game.add.text( (this.game.width/2) + 2, (this.game.height/2) + 2, text, style);
         ts.anchor.set(0.5);
