7df71a3c4e75a1f35851a4b601783cef914ccdc6
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2018-05-14 22:31:53 +0200

    Update php script

--- php/readMap.Preload.php
+++ php/readMap.Preload.php
@@ -1,11 +1,16 @@
 <?php
-$file = fopen("../assets/tilemaps/Level 0/ados/city.json", "r") or exit("Unable to open file!");
+
+$map = "../assets/tilemaps/Level 0/ados/city.json";
+
+$file = fopen($map, "r") or exit("Unable to open file!");
+
+$filePreload = "preload.txt";
+$fileGame = "game.txt";
 
 $process = false;
 
 while(!feof($file)) {
   	$line =  fgets($file);
-
 	
 	if( $process ) {
   		if ( preg_match("/\"image\"/", $line)) {
@@ -13,7 +18,6 @@ while(!feof($file)) {
 			$image2 = substr($image1[1], 1, -3);
 			$image3 = str_replace("\\", "", $image2);
 			$image4 = str_replace("../../", "assets/", $image3);
-			//echo "i: " . $image3 . "\n";
 		}
 		
 		if ( preg_match("/\"name\"/", $line)) {
@@ -23,8 +27,11 @@ while(!feof($file)) {
 			$name4 = str_replace(" ", "_", $name3);
 			$name5 = str_replace("../../", "", $name4);
 			$name6 = str_replace("/", "_", $name5);
-			//echo "n: " . $name3 . "\n";
-			echo "\t\tthis.load.image('" . $name6 . "', '" . $image4 . "');\n";
+			
+			$dataPreload = "\t\tthis.load.image('" . $name6 . "', '" . $image4 . "');\n";
+			$dataGame = "\t\tthis.map.addTilesetImage('" . $name2 . "', '" . $name6 . "');\n";
+			file_put_contents($filePreload, $dataPreload, FILE_APPEND | LOCK_EX);
+			file_put_contents($fileGame, $dataGame, FILE_APPEND | LOCK_EX);
 		}
 	}
 
