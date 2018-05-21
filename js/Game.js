6686bd4c5f17ac19cb450f333a0e352bb0a07d98
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2018-05-22 00:01:42 +0200

    Bad push

--- js/Game.js
+++ js/Game.js
@@ -60,10 +60,8 @@ CommandoZombi.Game.prototype = {
         // First argument: the tileset name as specified in Tiled; Second argument: the key to the asset
 
         asset = this.game.cache.getJSON('asset');
-        str = asset.level[0].tiled;
         for (i in asset.level) {
 			str = asset.level[i].tiled;
-			console.log(str + " " + asset.level[i].key);
             this.map.addTilesetImage(str, asset.level[i].key);
         }
 
